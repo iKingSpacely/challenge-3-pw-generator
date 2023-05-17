87bf2f688f4fa4c259c14011a942c8c0c6e00f88
Adam Colin <accolin89@gmail.com>
2023-05-17 15:24:43 -0500

    added in the variables at the top for special characters, numbers, lower and upper case letters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"]
-var numb = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowerCase = []
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
+var numb = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
 
 // Write password to the #password input
 function writePassword() {
